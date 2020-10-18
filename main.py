3fb09cdaf5b71445f4c1f14e53d7017f1999f0dc
Mickey <detrodoniv@yahoo.com>
2020-10-18 14:29:22 -0400

    rpg setup

---
+++ main.py
@@ -0,0 +1,7 @@
+from classes.game import Person, BCrolors
+
+
+magic = [{"name": "Fire", "cost": 10, "Dmg": 60},
+         {"name": "Thunder", "cost": 10, "Dmg": 80},
+         {"name": "Blizzard", "cost": 10, "Dmg": 60}]
+player = Person[450, 65, 60, 34, magic]
